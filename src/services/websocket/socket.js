9e2f8861bb2a8ae698db6bb94f707c749e3fb14d
Kanika Bhatia <99886418+Kanikabhatia08@users.noreply.github.com>
2026-05-22 11:49:37 +0530

    worked on with socket url

--- src/services/websocket/socket.js
+++ src/services/websocket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.1.10:7000", {
-// const socket = io("http://localhost:7000", {
+const socket = io("http://192.168.229.48:7000", {
+  // const socket = io("http://localhost:7000", {
 
   transports: ["websocket", "polling"],
   reconnection: true,
